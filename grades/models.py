3c4f816b1f59cf483d9ae037406feee4fb304f88
Ram Basnet <rambasnet@gmail.com>
2016-04-16 11:05:24 -0600

    modified models.py and template

--- grades/models.py
+++ grades/models.py
@@ -6,7 +6,11 @@ class Student(models.Model):
     last_name = models.CharField(max_length=50)
     test1 = models.FloatField(default=0)
     test2 = models.FloatField(default=0)
-    test3 = models.IntegerField(default=0)
+    test3 = models.FloatField(default=0)
+    avg = models.FloatField(default=0)
 
     def __str__(self):
         return self.first_name + " " + self.last_name
+
+    def findAverage(self):
+        self.avg = (self.test1+self.test2+self.test3)/3
